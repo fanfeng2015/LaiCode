c8360700e4d526f4bba32c69e39c557505101036
Fan Feng <fanfeng2015@gmail.com>
2018-07-27 15:19:26 -0700

    LeetCode N/A

--- ReconstructCompleteBinaryTreeWithLevelorder/src/reconstructCompleteBinaryTreeWithLevelorder/ReconstructCompleteBinaryTreeWithLevelorder.java
+++ ReconstructCompleteBinaryTreeWithLevelorder/src/reconstructCompleteBinaryTreeWithLevelorder/ReconstructCompleteBinaryTreeWithLevelorder.java
@@ -3,46 +3,26 @@ package reconstructCompleteBinaryTreeWithLevelorder;
 import java.util.ArrayList;
 import java.util.List;
 
-// How to re construct a complete binary tree from its level-order traversal sequence only.
+// How to reconstruct a complete binary tree from its level-order traversal sequence only.
 
-// Assumption:
-// 1. The given level-order is not null.
+// Assumption: The given level-order is not null.
 
 public class ReconstructCompleteBinaryTreeWithLevelorder {
 
 	public TreeNode construct(int[] level) {
-		List<Integer> list = new ArrayList<>();
+		int n = level.length;
+		List<TreeNode> list = new ArrayList<>();
 		for (int i : level) {
-			list.add(i);
+			list.add(new TreeNode(i));
 		}
-		return helper(list, 0);
-	}
-
-	private TreeNode helper(List<Integer> list, int index) {
-		// base case
-		if (index >= list.size()) {
-			return null;
+		for (int i = 0; i < n / 2; i++) {
+			TreeNode cur = list.get(i);
+			cur.left = list.get(2 * i + 1);
+			cur.right = (2 * i + 2 < n) ? list.get(2 * i + 2) : null;
 		}
-		TreeNode root = new TreeNode(list.get(index));
-		root.left = helper(list, 2 * index + 1);
-		root.right = helper(list, 2 * index + 2);
-		return root;
+		return level.length == 0 ? null : list.get(0);
 	}
 
 	// Time complexity is O(n).
-	// Space complexity is O(log(n)).
-
-	public static void main(String[] args) {
-		ReconstructCompleteBinaryTreeWithLevelorder reconstructCompleteBinaryTreeWithLevelorder = new ReconstructCompleteBinaryTreeWithLevelorder();
-		int[] level = new int[] { 1, 2, 3, 4, 5, 6, 7 };
-		TreeNode root = reconstructCompleteBinaryTreeWithLevelorder.construct(level);
-		System.out.println(root.key);
-		System.out.println(root.left.key);
-		System.out.println(root.right.key);
-		System.out.println(root.left.left.key);
-		System.out.println(root.left.right.key);
-		System.out.println(root.right.left.key);
-		System.out.println(root.right.right.key);
-	}
-
+	// Space complexity is O(1).
 }
