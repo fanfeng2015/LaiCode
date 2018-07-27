dcc186a53569a3078f185f198adcc168aba4ab0a
Fan Feng <fanfeng2015@gmail.com>
2018-07-27 16:59:02 -0700

    LeetCode N/A

--- ReconstructBSTWithPreorder/src/reconstructBSTWithPreorder/ReconstructBSTWithPreorder.java
+++ ReconstructBSTWithPreorder/src/reconstructBSTWithPreorder/ReconstructBSTWithPreorder.java
@@ -1,25 +1,26 @@
 package reconstructBSTWithPreorder;
 
-// Given the preorder traversal sequence of a binary search tree, reconstruct the original tree.
+// Given the pre-order traversal sequence of a binary search tree, reconstruct the original tree.
 
 // Assumption:
-// 1. The given sequence is not null
-// 2. There are no duplicate keys in the binary search tree
+// 1. The given sequence is not null.
+// 2. There are no duplicate keys in the binary search tree.
 
 public class ReconstructBSTWithPreorder {
 
+	private int index = 0;
+
 	public TreeNode reconstruct(int[] pre) {
-		int[] index = new int[] { 0 };
-		return helper(pre, index, Integer.MAX_VALUE);
+		return helper(pre, Integer.MAX_VALUE);
 	}
 
-	private TreeNode helper(int[] pre, int[] index, int max) {
-		if (index[0] >= pre.length || pre[index[0]] > max) {
+	private TreeNode helper(int[] pre, int max) {
+		if (index >= pre.length || pre[index] > max) {
 			return null;
 		}
-		TreeNode root = new TreeNode(pre[index[0]++]);
-		root.left = helper(pre, index, root.key);
-		root.right = helper(pre, index, max);
+		TreeNode root = new TreeNode(pre[index++]);
+		root.left = helper(pre, root.key);
+		root.right = helper(pre, max);
 		return root;
 	}
 
