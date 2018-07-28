90d6e1ca593c31df18830194adf06ff976d7cb1d
Fan Feng <fanfeng2015@gmail.com>
2018-07-27 17:01:29 -0700

    LeetCode N/A

--- ReconstructBSTWithPostorder/src/reconstructBSTWithPostorder/ReconstructBSTWithPostorder.java
+++ ReconstructBSTWithPostorder/src/reconstructBSTWithPostorder/ReconstructBSTWithPostorder.java
@@ -1,34 +1,31 @@
 package reconstructBSTWithPostorder;
 
-// Given the postorder traversal sequence of a binary search tree, reconstruct the original tree.
+// Given the post-order traversal sequence of a binary search tree, reconstruct 
+// the original tree.
 
 // Assumption:
-// 1. The given sequence is not null
-// 2. There are no duplicate keys in the binary search tree
+// 1. The given sequence is not null.
+// 2. There are no duplicate keys in the binary search tree.
 
 public class ReconstructBSTWithPostorder {
 
+	private int index = 0;
+
 	public TreeNode reconstruct(int[] post) {
-		int[] index = new int[] { post.length - 1 };
-		return helper(post, index, Integer.MIN_VALUE);
+		index = post.length - 1;
+		return helper(post, Integer.MIN_VALUE);
 	}
 
-	private TreeNode helper(int[] post, int[] index, int min) {
-		if (index[0] < 0 || post[index[0]] < min) {
+	private TreeNode helper(int[] post, int min) {
+		if (index < 0 || post[index] < min) {
 			return null;
 		}
-		TreeNode root = new TreeNode(post[index[0]--]);
-		root.right = helper(post, index, root.key);
-		root.left = helper(post, index, min);
+		TreeNode root = new TreeNode(post[index--]);
+		root.right = helper(post, root.key);
+		root.left = helper(post, min);
 		return root;
 	}
 
 	// Time complexity is O(n).
 	// Space complexity is O(n).
-	// Understand the reason of passing in int[] index, instead of int index
-
-	public static void main(String[] args) {
-		ReconstructBSTWithPostorder reconstructBSTWithPostorder = new ReconstructBSTWithPostorder();
-		reconstructBSTWithPostorder.reconstruct(new int[] { 1, 4, 3, 11, 8, 5 });
-	}
 }
