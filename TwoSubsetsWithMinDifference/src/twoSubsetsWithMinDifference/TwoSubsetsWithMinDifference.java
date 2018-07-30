dc2dced4e096428fbb742a87992d28f21d9347e2
Fan Feng <fanfeng2015@gmail.com>
2018-07-30 14:43:39 -0700

    LeetCode N/A

--- TwoSubsetsWithMinDifference/src/twoSubsetsWithMinDifference/TwoSubsetsWithMinDifference.java
+++ TwoSubsetsWithMinDifference/src/twoSubsetsWithMinDifference/TwoSubsetsWithMinDifference.java
@@ -6,21 +6,21 @@ import java.util.List;
 // Given a set of n integers, divide the set in two subsets (a: of size n/2 each, 
 // b: not necessarily of size n/2 each) such that the difference of the sum of two
 // subsets is as minimum as possible.
+
 // Return the minimum difference (absolute value).
 
 // Assumption: The given integer array is not null and it has length of >= 2.
 
 public class TwoSubsetsWithMinDifference {
 
-	// solution 1: DFS
+	private int min = Integer.MAX_VALUE, cur = 0, sum = 0;
+
+	// DFS
 	public int minDifference(int[] array) {
-		int sum = sum(array);
-		Integer[] total = new Integer[] { sum };
-		Integer[] min = new Integer[] { Integer.MAX_VALUE };
-		Integer[] cur = new Integer[] { 0 };
+		sum = sum(array);
 		List<Integer> list = new ArrayList<>();
-		DFS2(array, 0, total, min, cur, list);
-		return min[0];
+		DFS2(array, 0, list);
+		return min;
 	}
 
 	private int sum(int[] array) {
@@ -31,57 +31,59 @@ public class TwoSubsetsWithMinDifference {
 		return sum;
 	}
 
-	// Assumption: the two subsets don't need to be of about equal size
-	private void DFS(int[] array, int level, Integer[] total, Integer[] min, Integer[] cur, List<Integer> list) {
+	// Assumption: The two subsets don't need to be of about equal size.
+	private void DFS(int[] array, int level, List<Integer> list) {
 		if (level == array.length) {
-			// check if min needs to be updated
-			if (Math.abs(total[0] - 2 * cur[0]) < min[0]) {
-				min[0] = Math.abs(total[0] - 2 * cur[0]);
+			if (Math.abs(sum - 2 * cur) < min) {
+				min = Math.abs(sum - 2 * cur);
 			}
 			return;
 		}
+		cur += array[level];
 		list.add(array[level]);
-		DFS(array, level + 1, total, min, new Integer[] { cur[0] + array[level] }, list);
+		DFS(array, level + 1, list);
+		cur -= array[level];
 		list.remove(list.size() - 1);
-		DFS(array, level + 1, total, min, cur, list);
+		DFS(array, level + 1, list);
 	}
 
-	// Assumption: the two subsets are required to be of about equal size
-	private void DFS2(int[] array, int level, Integer[] total, Integer[] min, Integer[] cur, List<Integer> list) {
+	// Assumption: The two subsets need to be of about equal size.
+	private void DFS2(int[] array, int level, List<Integer> list) {
 		if (level == array.length) {
 			if ((array.length % 2 == 0 && list.size() == array.length / 2) || (array.length % 2 == 1
 					&& (list.size() == array.length / 2 || list.size() == array.length / 2 + 1))) {
-				if (Math.abs(total[0] - 2 * cur[0]) < min[0]) {
-					min[0] = Math.abs(total[0] - 2 * cur[0]);
+				if (Math.abs(sum - 2 * cur) < min) {
+					min = Math.abs(sum - 2 * cur);
 				}
 			}
 			return;
 		}
+		cur += array[level];
 		list.add(array[level]);
-		DFS2(array, level + 1, total, min, new Integer[] { cur[0] + array[level] }, list);
+		DFS2(array, level + 1, list);
+		cur -= array[level];
 		list.remove(list.size() - 1);
-		DFS2(array, level + 1, total, min, cur, list);
+		DFS2(array, level + 1, list);
 	}
 
 	// Time complexity is O(2^n).
 	// Space complexity is O(n).
 
-	// solution 2: DP
+	// DP
 	// Assumptions:
-	// 1. all elements are non-negative
-	// 2. the two subsets don't need to be of about equal size
-	// M[i][j]: whether elements in array, from index 0 to index i, inclusive
-	// can sum up to j
+	// 1. All elements are non-negative.
+	// 2. The two subsets don't need to be of about equal size.
+
+	// M[i][j]: whether elements in array [0, i] can sum to j
 	public int minDifference2(int[] array) {
 		int sum = sum(array);
 		boolean[][] matrix = new boolean[array.length][sum + 1];
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < sum + 1; j++) {
+			for (int j = 0; j <= sum; j++) {
 				if (i == 0) {
-					matrix[i][j] = (j == 0) ? true : (array[i] == j);
+					matrix[i][j] = (j == 0) || (array[i] == j);
 				} else {
-					matrix[i][j] = (j - array[i]) < 0 ? matrix[i - 1][j]
-							: matrix[i - 1][j] || matrix[i - 1][j - array[i]];
+					matrix[i][j] = matrix[i - 1][j] || (j - array[i] >= 0 && matrix[i - 1][j - array[i]]);
 				}
 			}
 		}
@@ -90,24 +92,11 @@ public class TwoSubsetsWithMinDifference {
 		while (!matrix[array.length - 1][index + offset] && !matrix[array.length - 1][index - offset]) {
 			offset++;
 		}
-		// matrix[array.length - 1][index + offset] || matrix[array.length -
-		// 1][index - offset]
-		int min = Integer.MAX_VALUE;
-		if (matrix[array.length - 1][index + offset]) {
-			min = Math.min(min, Math.abs(sum - 2 * (index + offset)));
-		}
-		if (matrix[array.length - 1][index - offset]) {
-			min = Math.min(min, Math.abs(sum - 2 * (index - offset)));
-		}
-		return min;
+		return Math.min(
+				(matrix[array.length - 1][index - offset]) ? Integer.MAX_VALUE : Math.abs(sum - 2 * (index - offset)),
+				(matrix[array.length - 1][index + offset]) ? Integer.MAX_VALUE : Math.abs(sum - 2 * (index + offset)));
 	}
 
-	// Time complexity is O(n*n*k), where [0, k] is range of elements
+	// Time complexity is O(n*n*k), where [0, k] is range of elements.
 	// Space complexity is O(n*n*k).
-
-	public static void main(String[] args) {
-		TwoSubsetsWithMinDifference twoSubsetsWithMinDifference = new TwoSubsetsWithMinDifference();
-		int[] array = new int[] { 1, 5, 2, 7 };
-		System.out.println(twoSubsetsWithMinDifference.minDifference2(array));
-	}
 }
